7e31cbad0a1a735665bab56f45ac5277845b37e9
antthoma <antthoma@student.42sp.org.br>
2022-06-21 00:21:19 +0200

    fix: removed files

---
+++ libft/ft_isdigit.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/13 02:44:57 by antthoma          #+#    #+#             */
+/*   Updated: 2022/06/13 22:09:27 by antthoma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
+
+/*
+	Function: ft_isdigit
+
+	check if character is digit
+	return 1 to true and 0 to false
+*/
