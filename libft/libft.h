6b113c6bbe076ad1b997f3634115c29b0f6b8142
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-06-21 23:58:28 -0300

    update prototypes
    Add new prototypes to new features

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 22:43:27 by antthoma          #+#    #+#             */
-/*   Updated: 2022/06/20 23:10:26 by antthoma         ###   ########.fr       */
+/*   Updated: 2022/06/21 20:19:20 by antthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,27 @@
 int	ft_isalpha(int c);
 
 /*
+
+*/
+int	ft_isprint(int c);
+
+/*
+
+*/
+int	ft_tolower(int c);
+
+/*
 	Function: ft_isdigit
 
 	check if character is digit
 	return 1 to true and 0 to false
 */
 int	ft_isdigit(int c);
+
+/*
+
+*/
+int	ft_toupper(int c);
 
 /*
 	Function:  ft_isalnum
@@ -58,7 +73,7 @@ size_t	ft_strlen(const char *s);
 	run an array and set value of value each position
 	just like calloc but with a customize value
 */	
-void	*memset(void *dest, int val, size_t len);
+void	*ft_memset(void *s, int c, size_t n);
 
 /*
 	Function: ft_bzero
@@ -82,6 +97,21 @@ void *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove (void *dest, const void *src, size_t n);
 
 /*
+
+*/
+char *ft_strchr(const char *s, int c);
+
+/*
+
+*/
+size_t	ft_strdump(const char *s);
+
+/*
+
+*/
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+
+/*
 	Function: ft_strlcpy
 
 	copy char from src to dst and check size
@@ -93,3 +123,43 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 	
 */
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+
+/*
+
+*/
+char *ft_strrchr(const char *s, int c);
+
+/*
+
+*/
+int ft_strncmp(const char *s1, const char *s2, size_t n);
+
+/*
+
+*/
+void	*ft_memchr(const void *s, int c, size_t n);
+
+/*
+
+*/
+int ft_memcmp(const void *s1, const void *s2, size_t n);
+
+/*
+
+*/
+int	ft_atoi(const char *nptr);
+
+/*
+
+*/
+void	*ft_calloc(size_t count, size_t size);
+
+/*
+
+*/
+char	*ft_strdup(const char *str);
+
+/*
+
+*/
+char	*ft_substr(char const *s, unsigned int start,size_t len);
