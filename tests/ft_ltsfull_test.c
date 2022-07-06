01c6b9554a5fba071f82ce1e75b594eca1cb458b
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-07-05 23:20:57 -0300

    feat: add full sample of t_list

---
+++ tests/ft_ltsfull_test.c
@@ -0,0 +1,130 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct s_list
+{
+	void *content;
+	struct s_list *next;
+} t_list;
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*first;
+
+	first = malloc(sizeof(*first));
+	if (!first)
+		return (NULL);
+	first->content = content;
+	first->next = NULL;
+	return (first);
+}
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	new->next = *lst;
+	*lst = new;
+}
+
+int	ft_lstsize(t_list *lst)
+{
+	size_t	len;
+
+	len = 0;
+	while (lst != NULL)
+	{
+		lst = lst->next;
+		len++;
+	}
+	return (len);
+}
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (0);
+	while (lst -> next != NULL)
+		lst = lst -> next;
+	return (lst);
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		temp = ft_lstlast(*lst);
+		temp->next = new;
+	}
+}
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst)
+		return ;
+	(*del)(lst->content);
+	free(lst);
+}
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	while (*lst != NULL)
+	{
+		tmp = *lst;
+		*lst = (*lst)->next;
+		del(tmp->content);
+		free(tmp);
+	}
+}
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst || !f)
+		return ;
+	while (lst)
+	{
+		(*f)(lst->content);
+		lst = lst->next;
+	}
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*list;
+	t_list	*newnode;
+
+	list = NULL;
+	while (lst)
+	{
+		newnode = ft_lstnew(f(lst->content));
+		if (!newnode)
+		{
+			ft_lstclear(&newnode, del);
+			return (NULL);
+		}
+		else
+			ft_lstadd_back(&list, newnode);
+		lst = lst->next;
+	}
+	return (list);
+}
+
+int main() {
+
+	int i;
+	t_list *l;
+
+	l = ft_lstnew("Hello");
+	i = 0;
+	while (i < 10 && l) {
+		printf("%s\n", (char *)l->content);
+		l = l->next;
+		i++;
+	}
+
+	return (0);
+}
